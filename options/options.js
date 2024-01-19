77b21a43548593f6140b21575b1fc15aa0780ccc
Crowdbotics <team@crowdbotics.com>
2024-01-19 19:08:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "markets-depot-45452",
+  url: "https://markets-depot-45452.botics.co",
+  api: "https://markets-depot-45452.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
